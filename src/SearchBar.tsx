2fe36d08ebde1f4870e8e01e02abb77d2fc87cc9
G. H. Krishna Chaitanya <g.h.krishnachaitanya@Gs-Mac-mini.local>
2023-04-02 14:50:53 +0530

    completed e_commerce search

--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -1,18 +1,61 @@
-import React,{useState,ChangeEvent} from 'react'
+import React,{useState,ChangeEvent,MouseEvent} from 'react'
+import { productList } from './productList'
 
 
 const SearchBar = () => {
     const [searchTerm,setSearchTerm] = useState('')
+    // 1. give type to state in <>
+    // 2. store only ids of filtered products in state
+    const [match,setMatch] = useState<number[]>([]);
     const readSearch = (e:ChangeEvent<HTMLInputElement>) => {
         setSearchTerm(e.target.value)
-        console.log(searchTerm)
     }
+    const handleSearch = (e:MouseEvent) => {
+        e.preventDefault()
+        const products = productList.filter(product => product.productName.toLowerCase().includes(searchTerm.toLowerCase()));
+        // 3. map the products to get array of ids of products and store it in match
+        console.log(products)
+        const productIds = products.map(product => product.id)
+        setMatch(productIds)
+        console.log(match)
+    }
+    // 4. get filtered products, using the product ids in match array
+    // function BookList() {
+    //     const [bookIds, setBookIds] = useState([1, 2, 3]);
+    //     const [books, setBooks] = useState([
+    //       { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+    //       { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee' },
+    //       { id: 3, title: 'Pride and Prejudice', author: 'Jane Austen' },
+    //     ]);
+      
+         const filteredProducts = productList.filter(product => match.includes(product.id));
+      
+    //     // Other code to render the book list
+    //   }
   return (
+    <div>
     <div className='flex justify-center gap-x-5 items-center ml-0'>
-      <input className='border-2 h-10 rounded-lg w-1/2 p-2 my-8'  onChange = {readSearch}/>
-      <button>Search</button>
+        <input className='border-2 h-10 rounded-lg w-1/2 p-2 my-8' value = {searchTerm}  onChange = {readSearch}/>
+        <button onClick  = {handleSearch}>Search</button>
+      </div>
+      {/* 5. use the new filtered products array to render h3s. */}
+      {filteredProducts.map(
+        item => (
+            <div key={item.id} className='text-center leading-relaxed'>
+                <h3><span className="font-bold">Product:</span> {item.productName}</h3>
+                <h3><span className="font-bold">quantity:</span> {item.quantity}</h3>
+                <h3><span className="font-bold">Price:</span> {item.price}</h3>
+                <h3><span className="font-bold">Description:</span> {item.productDescription}</h3>
+            </div>
+        )
+      )}
     </div>
   )
 }
+
+
+ 
+
+
 
 export default SearchBar
---
+++ src/productList.tsx
@@ -0,0 +1,55 @@
+export const productList = [
+    {
+        id:1,
+        productName:'tea',
+        quantity:"1/4 kg",
+        price:'200/-',
+        productDescription:'A soothing liquid that is made of tea leafs, water, milk and sugar'
+    },
+    {
+        id:2,
+        productName:'coffee',
+        quantity:"1/4 kg",
+        price:'100/-',
+        productDescription:'A caffen drink make up of coffee seeds powder, milk and sugar'
+    },
+    {
+        id:3,
+        productName:'Garam masala',
+        quantity:"1/2 kg",
+        price:'180/-',
+        productDesciption:'A taste enhancer used to add flavour to a dish'
+    },
+    {
+        id:4,
+        productName:'pepper seeds',
+        quantity:"1/4 kg",
+        price:'20/-',
+        productDescription:'A taste enhancer used add a strong spicy flavour to the dish, but we can tell the difference between the spice of a mirchi and pepper'
+
+    },
+    {
+        id:5,
+        productName:'Cashew',
+        quantity:"1/2 kg",
+        price:'500/-',
+        productDescription:'These are dry fruits (I think) that add crunch to the food and a bit of taste'
+
+    },
+    {
+        id:6,
+        productName:'cumin seeds',
+        quantity:"1/4 kg",
+        price:'50/-',
+        productDescription:'No data'
+
+    },
+    {
+        id:7,
+        productName:'mustard seeds',
+        quantity:"1/4 kg",
+        price:'40/-',
+        productDescription:'NO data'
+
+    },
+]
